a558c95648167899b46ce9ed551156dabfd4c820
jk15162428 <jk15162428@users.noreply.github.com>
2023-08-20 00:10:30 +0000

    deploy: ebde18a68f841ac21f7f0a05bb3f5c7f013f5a4b

--- js/hotkey.js
+++ js/hotkey.js
@@ -2,23 +2,19 @@
 const WEBPAGE_NAME = "The H-Group Conventions";
 const FIRST_DOC_PAGE_TITLE = "About";
 const LAST_DOC_PAGE_TITLE = "Convention Attribution";
-const MAX_LEVEL = 23;
+const MAX_LEVEL = 25;
 
 // Variables
 const keyMap = new Map();
 
-document.onkeydown = function onKeyDown(e) {
-  // Debugging
-  // console.log("Key pressed:", e.key);
-
-  // Do not do anything if we have any modifier keys pressed down
+document.addEventListener("keydown", (e) => {
+  // Do not do anything if we have any modifier keys pressed down.
   if (e.ctrlKey || e.shiftKey || e.altKey || e.metaKey) {
     return;
   }
 
-  // Do not do anything if we have the search box focused
-  const searchInputElements =
-    document.getElementsByClassName("DocSearch-Input");
+  // Do not do anything if we have the search box focused.
+  const searchInputElements = document.querySelectorAll(".DocSearch-Input");
   for (const searchInputElement of searchInputElements) {
     if (document.activeElement === searchInputElement) {
       return;
@@ -29,31 +25,31 @@ document.onkeydown = function onKeyDown(e) {
   if (keyFunction !== undefined) {
     keyFunction();
   }
-};
+});
 
-// Navigate backwards
+// Navigate backwards.
 keyMap.set("ArrowLeft", () => {
   if (isOnLandingPage()) {
     return;
   }
 
   if (isOnFirstDocPage()) {
-    // Click on the nav bar title
-    const navBarTitle = document.getElementsByClassName("navbar__title");
-    if (navBarTitle.length >= 1) {
+    // Click on the nav bar title.
+    const navBarTitle = document.querySelectorAll(".navbar__title");
+    if (navBarTitle.length > 0) {
       navBarTitle[0].click();
       return;
     }
   }
 
-  // Click on the left-most button
-  const buttons = document.getElementsByClassName("pagination-nav__link");
-  if (buttons.length >= 1) {
+  // Click on the left-most button.
+  const buttons = document.querySelectorAll(".pagination-nav__link");
+  if (buttons.length > 0) {
     buttons[0].click();
   }
 });
 
-// Navigate forwards
+// Navigate forwards.
 keyMap.set("ArrowRight", () => {
   if (isOnLandingPage()) {
     clickOnFirstLargeButton();
@@ -64,17 +60,18 @@ keyMap.set("ArrowRight", () => {
     return;
   }
 
-  // Otherwise, assume that we are on a doc page
-  const buttons = document.getElementsByClassName("pagination-nav__link");
+  // Otherwise, assume that we are on a doc page.
+  const buttons = document.querySelectorAll(".pagination-nav__link");
   if (buttons.length >= 2) {
     buttons[1].click();
-  } else if (buttons.length >= 1) {
+  } else if (buttons.length > 0) {
     buttons[0].click();
   }
 });
 
-// Go to a specific level
+// Go to a specific level.
 keyMap.set("l", () => {
+  // eslint-disable-next-line no-alert
   const levelString = window.prompt("Enter the level that you want to go to:");
   if (levelString === null || levelString === "") {
     return;
@@ -93,8 +90,8 @@ keyMap.set("l", () => {
 });
 
 function isOnLandingPage() {
-  const titles = document.getElementsByClassName("hero__title");
-  return titles.length >= 1;
+  const titles = document.querySelectorAll(".hero__title");
+  return titles.length > 0;
 }
 
 function isOnFirstDocPage() {
@@ -106,34 +103,35 @@ function isOnLastDocPage() {
 }
 
 function clickOnFirstLargeButton() {
-  const largeButtons = document.getElementsByClassName("button--lg");
-  if (largeButtons.length >= 1) {
+  const largeButtons = document.querySelectorAll(".button--lg");
+  if (largeButtons.length > 0) {
     largeButtons[0].click();
   }
 }
 
-// parseIntSafe is a more reliable version of parseInt
-// By default, "parseInt('1a')" will return "1", which is unexpected
-// This returns either an integer or NaN
+/**
+ * `parseIntSafe` is a more reliable version of `parseInt`. By default, "parseInt('1a')" will return
+ * "1", which is unexpected This returns either an integer or NaN.
+ */
 function parseIntSafe(input) {
-  // Remove all leading and trailing whitespace
+  // Remove all leading and trailing whitespace.
   let trimmedInput = input.trim();
 
   const isNegativeNumber = trimmedInput.startsWith("-");
   if (isNegativeNumber) {
-    // Remove the leading minus sign before we match the regular expression
-    trimmedInput = trimmedInput.substring(1);
+    // Remove the leading minus sign before we match the regular expression.
+    trimmedInput = trimmedInput.slice(1);
   }
 
   if (/^\d+$/.exec(trimmedInput) === null) {
-    // "\d" matches any digit (same as "[0-9]")
-    return NaN;
+    // "\d" matches any digit (same as "[0-9]").
+    return Number.NaN;
   }
 
   if (isNegativeNumber) {
-    // Add the leading minus sign back
+    // Add the leading minus sign back.
     trimmedInput = `-${trimmedInput}`;
   }
 
-  return parseInt(trimmedInput, 10);
+  return Number.parseInt(trimmedInput, 10);
 }
